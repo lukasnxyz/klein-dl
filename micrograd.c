a8f98362dc6156926e91296ffb93e2be48a15166
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-07-17 11:08:28 -0500

    working on topo

--- micrograd.c
+++ micrograd.c
@@ -10,17 +10,57 @@ struct Val {
 };
 
 struct Tnode {
-	Val *val;
+	struct Val *val;
 	struct Tnode *next;
 };
 
-void topo_append(Tnode **topo, Val *val) {
+void topo_prepend(struct Tnode **topo, struct Val **val) {
+	struct Tnode *n_tnode = (struct Tnode *)malloc((sizeof(struct Tnode)));
+	n_tnode->val  = *val;
+	n_tnode->next = *topo;
+	*topo = n_tnode;
 }
 
-void topo_build() {
+void topo_build(struct Val **val, struct Tnode **topo, struct Val **visited, size_t *visited_count) {
+	for (size_t i = 0; i < *visited_count; i++) {
+		if (visited[i] == *val) {
+			return;
+		}
+	}
+
+	visited[*visited_count] = *val;
+	(*visited_count)++;
+
+	for (size_t i = 0; i < 2; i++) {
+		topo_build((*val)->children[i], topo, visited, visited_count);
+	}
+
+	topo_prepend(topo, val);
 }
 
-void topo_free() {
+void topo_free(struct Tnode *topo) {
+	struct Tnode *current = topo;
+	while (current != NULL) {
+		struct Tnode *next = current->next;
+		free(current);
+		current = next;
+		}
+}
+
+void val_backward(struct Val **val) {
+	struct Tnode *topo = NULL;
+	struct Val **visited = (struct Val **)malloc(sizeof(struct Val *) * 100); /* assuming max 100 vals */
+	size_t visited_count = 0;
+
+	(*val)->grad = 1.0;
+	topo_build(val, &topo, visited, &visited_count);
+
+	for (size_t i = visited_count; i > 0; i--) {
+		topo[i].val->backward(topo[i].val->children[0], topo[i].val->children[1], &topo[i].val);
+	}
+
+	free(visited);
+	topo_free(topo);
 }
 
 // mul backward
@@ -60,19 +100,12 @@ struct Val *val_add(struct Val **val1, struct Val **val2) {
 }
 // struct Val *val_mul
 
-void val_backward(struct Val **val) {
-	// topo = {}
-
-	// for node in topo
-	// 		node.backward
-}
-
 int main(void) {
 	struct Val *val1 = val_init(2.5);
 	struct Val *val2 = val_init(3.5);
 	struct Val *val3 = val_add(&val1, &val2);
-	val3->grad = 1.0;
-	val3->backward(val3->children[0], val3->children[1], &val3);
+
+	val_backward(&val3);
 
 	val_print(*val1);
 	val_print(*val2);
