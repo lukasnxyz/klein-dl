097ff96c01858a2289786d3cdd12a4c6730110dd
lukasnxyz <lukasnitzsche@yahoo.com>
2024-08-12 11:51:13 +0200

    getting some things working and cleaning up/minimalizing

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 ## GOAL: 
 - very minimal ml framework
 - read papers and implement them into this framework (like adam, dropout, etc.)
+- implement apple accelerator (mps)
+- visual graph of all tensors (see micrograd)
 
 ## NAME IDEAS: 
 - klein-ml
+- klein-dl
+- kleindl
 - sprout-ml
+- steigung
--- src/tensor.py
+++ src/tensor.py
@@ -1,6 +1,7 @@
 # inspired by: https://github.com/tinygrad/tinygrad/blob/c900b6e/tinygrad/tensor.py
 import numpy as np
 from functools import partialmethod
+from typing import Union, List
 
 class Ctx:
     def __init__(self, arg, *tensors):
@@ -13,9 +14,13 @@ class Ctx:
         self.saved_tensors.extend(x)
 
 class Tensor():
-    def __init__(self, data):
-        # make throw error if not np.ndarray
-        self.data = data if type(data) == np.ndarray else np.array(data)
+    def __init__(self, data: Union[np.ndarray, List, float], dtype=np.float32):
+        # clean this up
+        if type(data) == np.ndarray:
+            self.data = data.astype(dtype)
+        else: 
+            print('[WARNING]: make sure all arrays are of type np.ndarray')
+            np.array(data, dtype=dtype)
         self.grad = None
         self._ctx = None
 
@@ -37,13 +42,17 @@ class Tensor():
         if len(self._ctx.parents) == 1:
             grads = [grads] # wrap single gradient in a list
         for t, g in zip(self._ctx.parents, grads):
-            if g.shape != t.data.shape:
-                print('grad shape must match tensor shape')
+            # TODO: shape check doesn't work
+            if g.shape != t.data.shape: 
+                print('grad shape does not match tensor shape')
                 assert(False)
             t.grad = g # gradient of parent tensor
             t.backward(False) # recursivly call backward on parent
 
 class Function:
+    def forward(ctx, *args): raise NotImplementedError('forward not implemented for function')
+    def backward(ctx, grad_output): raise NotImplementedError('backward not implemented for function')
+
     def apply(self, arg, *x):
         ctx = Ctx(arg, self, *x)
         # call forward of func and create new tensor with result
@@ -51,29 +60,37 @@ class Function:
         ret._ctx = ctx 
         return ret
 
-def register(name, fxn):
-    # add method to Tensor class using partialmethod
-    setattr(Tensor, name, partialmethod(fxn.apply, fxn))
+def register_function(name):
+    def decorator(cls):
+        setattr(Tensor, name, partialmethod(cls.apply, cls))
+        return cls
+    return decorator
 
-# Mul
-# Add
-# ReLU
-# LogSoftmax
+# Add, ReLU, LogSoftmax
 
-class Dot(Function):
-    @staticmethod
-    def forward(ctx, inp, weight):
-        ctx.save_for_backward(inp, weight)
-        return inp.dot(weight)
-    
-    @staticmethod
+@register_function('mul')
+class Mul(Function):
+    def forward(ctx, x, y):
+        ctx.save_for_backward(x, y)
+        return x*y
+
     def backward(ctx, grad_output):
-        inp, weight = ctx.saved_tensors # get saved tensors
-        grad_input = grad_output.dot(weight.T) # grad dot weight
-        grad_weight = grad_output.T.dot(inp).T # grad dot inp
-        return grad_input, grad_weight
-register('dot', Dot) # register Dot function as a method of Tensor
+        x, y = ctx.saved_tensors
+        return y*grad_output, x*grad_output
 
+@register_function('dot')
+class Dot(Function):
+    def forward(ctx, x, y):
+        ctx.save_for_backward(x, y)
+        return x.dot(y)
+    
+    def backward(ctx, grad_output):
+        x, y = ctx.saved_tensors # get saved tensors
+        grad_input = grad_output.dot(y.T) 
+        grad_weight = grad_output.T.dot(x).T 
+        return grad_input, grad_weight
+
+# ------------------------------------------------------------------------------
 if __name__ == '__main__':
     ts = [
         Tensor(np.random.uniform(-1., 1., size=(1,5))),
@@ -86,10 +103,9 @@ if __name__ == '__main__':
     ts[2].backward()
     print('--- backward ---')
     print(ts[0].grad, ts[1].grad, ts[2].grad)
-
+    
     print()
     for t in ts:
         if t._ctx is not None:
             for p in t._ctx.parents:
-                print(t._ctx.arg)
                 print('p', p)
